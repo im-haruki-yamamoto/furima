8e1d4efe750b184663e6d08c808a1b78f8df1a4d
TA0608 <t.amano@infomart.co.jp>
2026-08-13 17:27:01 +0900

    一覧機能と出品機能

---
+++ src/main/java/in/tech_camp/furima/entity/ItemEntity.java
@@ -0,0 +1,19 @@
+package in.tech_camp.furima.entity;
+
+import lombok.Data;
+
+@Data
+public class ItemEntity {
+
+  private Long id;
+  private Long userId;
+  private String name;
+  private String description;
+  private Integer category;
+  private Integer condition;
+  private Integer deliveryFee;
+  private Integer prefecture;
+  private Integer untilDelivery;
+  private Long price;
+  private String img;
+}
---
+++ src/main/java/in/tech_camp/furima/service/ItemService.java
@@ -0,0 +1,84 @@
+package in.tech_camp.furima.service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import in.tech_camp.furima.dto.ItemListDto;
+import in.tech_camp.furima.entity.ItemEntity;
+import in.tech_camp.furima.enums.DeliveryFeeType;
+import in.tech_camp.furima.form.ItemForm;
+import in.tech_camp.furima.repository.ItemRepository;
+
+@Service
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+      public ItemService(ItemRepository itemRepository) {
+    this.itemRepository = itemRepository;
+  }
+
+    // 商品一覧表示機能
+  public List<ItemListDto> allItem() {
+
+    List<ItemListDto> items = itemRepository.findAll()
+        .stream().map(item -> {
+          ItemListDto dto = new ItemListDto();
+          dto.setId(item.getId());
+          dto.setImg(item.getImg());
+          dto.setName(item.getName());
+          dto.setPrice(item.getPrice());
+          dto.setSoldout(item.getProductId() != null);
+          dto.setDeliveryFee(DeliveryFeeType.fromCode(item.getDeliveryFee()).getLabel());
+          return dto;
+        }).collect(Collectors.toList());
+
+    return items;
+  }
+
+    // 商品出品機能
+  @Transactional
+  public void saveItem(ItemForm form, Long userId) throws IOException {
+    String imageName = null;
+    MultipartFile imgFile = form.getImg();
+
+    if (imgFile != null && !imgFile.isEmpty()) {
+      String uuid = UUID.randomUUID().toString();
+      imageName = uuid + "-" + imgFile.getOriginalFilename();
+
+      Path uploadDir = Paths.get("uploads").toAbsolutePath();
+
+      if (!Files.exists(uploadDir)) {
+        Files.createDirectories(uploadDir);
+      }
+
+      Path imagePath = uploadDir.resolve(imageName);
+      Files.copy(imgFile.getInputStream(), imagePath);
+    }
+
+    ItemEntity item = new ItemEntity();
+    item.setImg(imageName);
+    item.setUserId(userId);
+    item.setName(form.getName());
+    item.setDescription(form.getDescription());
+    item.setCategory(form.getCategory());
+    item.setCondition(form.getCondition());
+    item.setDeliveryFee(form.getDeliveryFee());
+    item.setPrefecture(form.getPrefecture());
+    item.setUntilDelivery(form.getUntilDelivery());
+    item.setPrice(form.getPrice());
+
+    itemRepository.insert(item);
+  }
+
+
+}
