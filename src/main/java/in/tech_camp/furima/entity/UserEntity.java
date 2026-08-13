b7b441b2b93b0fa2db61d5539e03e4892549feaa
Hyeona9 <h.kuriki@infomart.co.jp>
2026-08-13 15:19:19 +0900

    EntityとDto

---
+++ src/main/java/in/tech_camp/furima/entity/UserEntity.java
@@ -0,0 +1,15 @@
+package in.tech_camp.furima.entity;
+
+import lombok.Data;
+
+@Data
+public class UserEntity {
+  private Long id;
+  private String nickname;
+  private String email;
+  private String password;
+  private String lastName;
+  private String firstName;
+  private String lastNameKana;
+  private String firstNameKana;
+}
