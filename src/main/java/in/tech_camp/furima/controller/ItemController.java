281bbfc43ad3f0d41dfc9cea3bad42119740e580
Hyeona9 <h.kuriki@infomart.co.jp>
2026-08-13 15:49:44 +0900

    リダイレクト対応

--- src/main/java/in/tech_camp/furima/controller/ItemController.java
+++ src/main/java/in/tech_camp/furima/controller/ItemController.java
@@ -3,6 +3,7 @@ package in.tech_camp.furima.controller;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import in.tech_camp.furima.dto.ItemEditDto;
+import in.tech_camp.furima.exception.ForbiddenException;
+import in.tech_camp.furima.exception.ResourceNotFoundException;
 import in.tech_camp.furima.service.ItemService;
 import lombok.RequiredArgsConstructor;
 
@@ -27,9 +30,13 @@ public class ItemController {
       @PathVariable Long id,
       @AuthenticationPrincipal CustomUserDetails userDetails, Model model) {
 
-    ItemEditDto itemEditDto = itemService.getItemForEdit(id, userDetails.getId());
-    model.addAttribute("itemForm", itemEditDto);
-    return "items/edit";
+    try {
+      ItemEditDto itemEditDto = itemService.getItemForEdit(id, userDetails.getId());
+      model.addAttribute("itemForm", itemEditDto);
+      return "items/edit";
+    } catch (ResourceNotFoundException | ForbiddenException e) {
+      return "redirect:/";
+    }
   }
 
   // 編集実行
@@ -37,10 +44,19 @@ public class ItemController {
   public String updateItem(
       @PathVariable Long id,
       @ModelAttribute("itemForm") ItemEditDto itemEditDto,
+      BindingResult bindingResult,
       @RequestParam(value = "image", required = false) MultipartFile image,
       @AuthenticationPrincipal CustomUserDetails userDetails) {
 
-    itemService.updateItem(id, itemEditDto, image, userDetails.getId());
-    return "redirect:/items/" + id;
+    if (bindingResult.hasErrors()) {
+      return "items/edit";
+    }
+
+    try {
+      itemService.updateItem(id, itemEditDto, image, userDetails.getId());
+      return "redirect:/items/" + id;
+    } catch (ResourceNotFoundException | ForbiddenException e) {
+      return "redirect:/";
+    }
   }
 }
