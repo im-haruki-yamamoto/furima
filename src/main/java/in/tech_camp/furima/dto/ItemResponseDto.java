1df44f2c5eff50ea3b2082abd59302f277606945
s-nishimura06 <s.nisimura@infomart.co.jp>
2026-08-17 14:40:45 +0900

    コンフリクト解消

--- src/main/java/in/tech_camp/furima/dto/ItemResponseDto.java
+++ src/main/java/in/tech_camp/furima/dto/ItemResponseDto.java
@@ -1,48 +1,51 @@
 package in.tech_camp.furima.dto;
 
 import in.tech_camp.furima.entity.ItemEntity;
-import in.tech_camp.furima.entity.UserEntity;
 import in.tech_camp.furima.enums.Category;
 import in.tech_camp.furima.enums.Condition;
 import in.tech_camp.furima.enums.DeliveryFeeType;
 import in.tech_camp.furima.enums.PrefectureType;
 import in.tech_camp.furima.enums.UntilDelivery;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class ItemResponseDto {
-  private Long id;
-  private String name;
-  private String description;
-  private String categoryText;
-  private String conditionText;
-  private String deliveryFeeText;
-  private String prefectureText;
-  private String untilDeliveryText;
-  private Long price;
-  private String img;
-  private Long userId;
-  private String userNickname;
+    private Long id;
+    private String name;
+    private String description;
+    private String categoryText;
+    private String conditionText;
+    private String deliveryFeeText;
+    private String prefectureText;
+    private String untilDeliveryText;
+    private Long price;
+    private String img;
+    private Long userId;
 
-  public ItemResponseDto(ItemEntity item, UserEntity user) {
-    this.id = item.getId();
-    this.name = item.getName();
-    this.description = item.getDescription();
-    this.price = item.getPrice();
-    this.img = item.getImg();
-    
+    public ItemResponseDto(ItemEntity item) {
+        this.id = item.getId();
+        this.name = item.getName();
+        this.description = item.getDescription();
+        this.price = item.getPrice();
+        this.img = item.getImg();
+        this.userId = item.getUserId();
 
-    this.userId = item.getUserId();
-    
-
-    if (user != null) {
-      this.userNickname = user.getNickname();
+        if (item.getCategory() != null) {
+            this.categoryText = Category.fromCode(item.getCategory()).getDisplayName();
+        }
+        if (item.getDeliveryFee() != null) {
+            this.deliveryFeeText = DeliveryFeeType.fromCode(item.getDeliveryFee()).getDisplayName();
+        }
+        if (item.getPrefecture() != null) {
+            this.prefectureText = PrefectureType.fromCode(item.getPrefecture()).getDisplayName();
+        }
+        if (item.getCondition() != null) {
+            this.conditionText = Condition.fromCode(item.getCondition()).getDisplayName();
+        }
+        if (item.getUntilDelivery() != null) {
+            this.untilDeliveryText = UntilDelivery.fromCode(item.getUntilDelivery()).getDisplayName();
+        }
     }
-
-    this.categoryText = Category.fromCode(item.getCategory()).getDisplayName();
-    this.deliveryFeeText = DeliveryFeeType.fromCode(item.getDeliveryFee()).getDisplayName();
-    this.prefectureText = PrefectureType.fromCode(item.getPrefecture()).getDisplayName();
-    this.conditionText = Condition.fromCode(item.getCondition()).getDisplayName();
-    this.untilDeliveryText = UntilDelivery.fromCode(item.getUntilDelivery()).getDisplayName();
-  }
 }
