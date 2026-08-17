7459b457382fd74ffe414fe2fa3b4f18194fbcb0
im_hibiki_kuriki <h.kuriki@infomart.co.jp>
2026-08-17 09:40:39 +0900

    Fix git cache-tree index error and update sign_up.html

--- src/main/java/in/tech_camp/furima/config/SecurityConfig.java
+++ src/main/java/in/tech_camp/furima/config/SecurityConfig.java
@@ -1,15 +1,13 @@
 package in.tech_camp.furima.config;
 
-import org.springframework.http.HttpMethod;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
 
 @Configuration
 @EnableWebSecurity
@@ -20,8 +18,9 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                       .requestMatchers("/css/**", "/images/**", "/", "/users/sign_up", "/users/sign_in", "/error").permitAll()
-                       .anyRequest().authenticated())
+                        .requestMatchers("/css/**", "/images/**", "/", "/users/sign_up", "/users/sign_in", "/error")
+                        .permitAll()
+                        .anyRequest().authenticated())
 
                 .formLogin(login -> login
                         .loginProcessingUrl("/users/sign_in")
@@ -35,6 +34,7 @@ public class SecurityConfig {
                         .logoutSuccessUrl("/"));
         return http.build();
     }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
